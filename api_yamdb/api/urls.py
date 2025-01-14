ac882164da9e7fdb2569b91d51b5e520757ba069
Maxim Petrov <mail@maximpetrov.ru>
2025-01-14 23:03:33 +0300

    Fix views

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (CategoryViewSet, CommentViewSet,
-                       GenreViewSet, ReviewViewSet, TitleViewSet)
-from users.views import MeView, SignUpView, TokenObtainView, UserViewSet
+                       GenreViewSet, ReviewViewSet, TitleViewSet, MeView,
+                       SignUpView, TokenObtainView, UserViewSet)
 
 api_v1_router = DefaultRouter()
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,23 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, viewsets
+from rest_framework import filters, generics, status, viewsets, mixins
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.filters import TitleFilter
 from api.paginations import ReviewPagination
-from api.permissions import IsAdminOrReadOnly, IsAuthorAdminModerOrReadOnly
+from api.permissions import (IsAdminOrReadOnly, IsAuthorAdminModerOrReadOnly,
+                             IsAdmin)
 from api.serializers import (CategorySerializer, CommentSerializer,
                              GenreSerializer, ReviewSerializer,
-                             TitleReadSerializer, TitleWriteSerializer)
+                             TitleReadSerializer, TitleWriteSerializer,
+                             AdminUserSerializer, MeSerializer,
+                             SignUpSerializer, TokenSerializer)
 from reviews.models import Category, Genre, Review, Title
+from users.models import User
 
 
 class GenreViewSet(
@@ -121,3 +128,72 @@ class CommentViewSet(viewsets.ModelViewSet):
                 "PUT", detail="Метод PUT не разрешен."
             )
         return super().update(request, *args, **kwargs)
+
+class SignUpView(generics.CreateAPIView):
+    """
+    Представление для регистрации пользователя.
+    При POST-запросе с email и username отправляет код подтверждения.
+    """
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        """Обрабатывает POST-запрос, создаёт пользователя, отправляет код."""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(
+            {'email': user.email, 'username': user.username},
+            status=status.HTTP_200_OK
+        )
+
+
+class TokenObtainView(APIView):
+    """
+    Представление для получения JWT-токена.
+    Принимает username и confirmation_code, возвращает токен.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        """Обрабатывает POST-запрос и возвращает JWT-токен."""
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для управления пользователями.
+    Доступен только администратору.
+    """
+    queryset = User.objects.all()
+    serializer_class = AdminUserSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ('username',)
+    lookup_field = 'username'
+
+    def update(self, request, *args, **kwargs):
+        """Запрещаем PUT-запросы (Method Not Allowed)."""
+        if request.method.upper() == 'PUT':
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """PATCH-запрос с валидацией данных."""
+        return super().partial_update(request, *args, **kwargs)
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    """
+    Представление для просмотра и изменения своих данных (профиля).
+    Доступно только аутентифицированному пользователю.
+    """
+    serializer_class = MeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        """Возвращает текущего аутентифицированного пользователя."""
+        return self.request.user
