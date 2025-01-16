8daa61ff1ed55c0a6cf25d956ae6830a6bc7e975
Alex F <feyginalex@gmail.com>
2025-01-16 23:03:43 +0300

    fixed pep8 formatting issues

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import Avg
 
 from reviews.constants import (TEXT_MAX_LENGTH, SLUG_MAX_LENGTH,
                                MIN_SCORE, MAX_SCORE)
